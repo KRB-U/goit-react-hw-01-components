8af6ea9cc0806244f2be1b1684961e5d26c7d53a
unknown <rubikonspace@ukr.net>
2023-09-30 15:01:09 +0300

    css для FriendList isOnline

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,11 +5,10 @@ const FriendList = ({ friends }) => {
     <>
       <ul className={css.friendList}>
         {friends.map(friend => {
+          const isOnline = friend.isOnline ? css.true : css.false;
           return (
             <li key={friend.id} className={css.item}>
-              <span
-                className={friend.isOnline ? css.online : css.offline}
-              ></span>
+              <span className={`${css.status} ${isOnline}`}></span>
               <img
                 className={css.avatar}
                 src={friend.avatar}
